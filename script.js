d74ece8be2a80932d684d5a8f4e6acd4a1206664
Esthervh1 <145069068+Esthervh1@users.noreply.github.com>
2023-11-27 14:44:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function mudarCor() {
+    var minhaDiv = document.getElementById("myBox");
+    minhaDiv.style.backgroundColor = "yellow";
+}
+
+function mudardevolta() {
+    var minhaDiv = document.getElementById("myBox");
+    minhaDiv.style.backgroundColor = "lightblue";
+}
